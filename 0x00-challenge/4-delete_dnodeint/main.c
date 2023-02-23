fff036ed3ecb9371c907a00cd1951f6fb7498c9c
Emmaxcom <emmaxcom@yahoo.com>
2023-02-23 08:39:54 -0800

    task 4

--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -3,16 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
- */
 int main(void)
 {
     dlistint_t *head;
-
     head = NULL;
+
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
@@ -59,11 +54,9 @@ int main(void)
     delete_dnodeint_at_index(&head, 0);
     printf("-----------------\n");
     delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
-    printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
     print_dlistint(head);
     free_dlistint(head);
+
     return (0);
 }
+
